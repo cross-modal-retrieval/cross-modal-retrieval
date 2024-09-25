1e9bbe8a437082c24b46edc527882773b1021830
cross-modal-retrieval <991126843@qq.com>
2024-09-25 17:44:20 +0800

    Delete Code-repository/Text-image cross-modal retrieval/Unsupervised real-value retrieval/MCSM directory

--- Code-repository/Text-image cross-modal retrieval/Unsupervised real-value retrieval/MCSM/MCSM_TIP2018-master/calMAP/mnorm.m
+++
@@ -1,6 +0,0 @@
-function [tr_norm] = mnorm(tr)
-
-maxtr = (max(tr'))';
-mintr = (min(tr'))';
-tr_norm = (tr - repmat(mintr, 1, size(tr,2))) ./ (repmat((maxtr - mintr), 1, size(tr,2)));
-tr_norm(find(isnan(tr_norm))) = 0;
--- Code-repository/Text-image cross-modal retrieval/Unsupervised real-value retrieval/MCSM/MCSM_TIP2018-master/i2t_attention/extracted_feature/README.txt
+++
@@ -1,1 +0,0 @@
-The extracted feature (including image and text feature) will be saved here, which will be used for mAP calculating.
